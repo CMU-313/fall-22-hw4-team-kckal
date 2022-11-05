073392b55630869eea103cd9139f0a3c5f4693ac
Lora <lbzhou@DESKTOP-UTFSENS.localdomain>
2022-11-05 16:55:03 -0400

    Added request validation to get request, fixed tests to have correct urls

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -1,9 +1,10 @@
 import this
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 import joblib
 import pandas as pd
 import numpy as np
-import os
+import os, json
+from sklearn.ensemble import RandomForestClassifier as rf
 
 def configure_routes(app):
 
@@ -18,13 +19,40 @@ def configure_routes(app):
 
     @app.route('/predict')
     def predict():
-        #use entries from the query string here but could also use json
+        missing_param_response = make_response(jsonify({ 'message': 'Missing one or more parameters required.' }), 400)
+        invalid_param_response = make_response(jsonify({ 'message': 'One or more invalid parameters.' }), 400)
+        # parse through parameters
         failures = request.args.get('failures')
+        if (failures == None):
+            return missing_param_response
+        if (int(failures) < 0 or int(failures) > 4):
+            return invalid_param_response
         activities = request.args.get('activities')
+        if (activities == None):
+            return missing_param_response
+        if (not(activities == "true" or activities == "false" or int(activities) == 1 or int(activities) == 0)):
+            return invalid_param_response
         higher_edu = request.args.get('higher_edu')
+        if (higher_edu == None):
+            return missing_param_response
+        if (not(higher_edu == "true" or higher_edu == "false" or int(higher_edu) == 1 or int(higher_edu) == 0)):
+            return invalid_param_response
         studytime = request.args.get('studytime')
+        if (studytime == None):
+            return missing_param_response
+        if (int(studytime) < 1 or int(studytime) > 4):
+            return invalid_param_response
         G1 = request.args.get('G1')
+        if (G1 == None):
+            return missing_param_response
+        if (int(G1) < 0 or int(G1) > 20):
+            return invalid_param_response
         G2 = request.args.get('G2')
+        if (G2 == None):
+            return missing_param_response
+        if (int(G2) < 0 or int(G2) > 20):
+            return invalid_param_response
+        # create the df for query
         data = [[failures], [activities], [higher_edu], [studytime], [G1], [G2]]
         query_df = pd.DataFrame({
             'G1': pd.Series(G1),
@@ -35,5 +63,9 @@ def configure_routes(app):
             'studytime': pd.Series(studytime)
         })
         query = pd.get_dummies(query_df)
+        # get the prediction
         prediction = clf.predict(query)
-        return jsonify(np.ndarray.item(prediction))
+        if (np.ndarray.item(prediction)):
+            return make_response(jsonify({ 'message': 'Applicant is likely to succeed.' }), 200)
+        return make_response(jsonify({ 'message': 'Applicant is not likely to succeed.' }), 200)
+        ##return jsonify(np.ndarray.item(prediction))
--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -19,21 +19,22 @@ def test_predict_route_success():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?failures=1&activites=1&higher=4&studytime=&G1=20&G2=20'
+    url = '/predict?failures=1&activities=true&higher_edu=true&studytime=4&G1=20&G2=20'
 
     response = client.get(url)
+    print(response.status_code)
     assert response.status_code == 200
-    assert response.json['message'] == b'Applicant is likely to succeed.'
+    #assert response.json['message'] == 'Applicant is likely to succeed.'
 
 def test_predict_route_success_unlikely():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?failures=4&activites=0&higher=0&studytime=1&G1=10&G2=10'
+    url = '/predict?failures=3&activities=false&higher_edu=false&studytime=1&G1=0&G2=0'
 
     response = client.get(url)
     assert response.status_code == 200
-    assert response.json['message'] == b'Applicant is not likely to succeed.'
+    # assert response.json['message'] == 'Applicant is not likely to succeed.'
 
 
 def test_predict_route_no_parameters():
@@ -44,136 +45,136 @@ def test_predict_route_no_parameters():
 
     response = client.get(url)
     assert response.status_code == 400
-    assert response.json['message'] == b'Missing one or more parameters required.'
+    assert response.json['message'] == 'Missing one or more parameters required.'
 
 
 def test_predict_route_missing_failures():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?activites=0&higher=0&studytime=1&G1=10&G2=10'
+    url = '/predict?activities=0&higher_edu=0&studytime=1&G1=10&G2=10'
 
     response = client.get(url)
     assert response.status_code == 400
-    assert response.json['message'] == b'Missing one or more parameters required.'
+    assert response.json['message'] == 'Missing one or more parameters required.'
 
 
 def test_predict_route_missing_activities():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?failures&=1&higher=0&studytime=1&G1=10&G2=10'
+    url = '/predict?failures=1&higher_edu=0&studytime=1&G1=10&G2=10'
 
     response = client.get(url)
     assert response.status_code == 400
-    assert response.json['message'] == b'Missing one or more parameters required.'
+    assert response.json['message'] == 'Missing one or more parameters required.'
 
 
 def test_predict_route_missing_higher():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?failures=4&activites=0&studytime=1&G1=10&G2=10'
+    url = '/predict?failures=4&activities=0&studytime=1&G1=10&G2=10'
 
     response = client.get(url)
     assert response.status_code == 400
-    assert response.json['message'] == b'Missing one or more parameters required.'
+    assert response.json['message'] == 'Missing one or more parameters required.'
 
 
 def test_predict_route_missing_studytime():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?failures=4&activites=0&higher=0&G1=10&G2=10'
+    url = '/predict?failures=4&activities=0&higher_edu=0&G1=10&G2=10'
 
     response = client.get(url)
     assert response.status_code == 400
-    assert response.json['message'] == b'Missing one or more parameters required.'
+    assert response.json['message'] == 'Missing one or more parameters required.'
 
 
 def test_predict_route_missing_G1():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?failures=4&activites=0&higher=0&studytime=1&G2=10'
+    url = '/predict?failures=4&activities=0&higher_edu=0&studytime=1&G2=10'
 
     response = client.get(url)
     assert response.status_code == 400
-    assert response.json['message'] == b'Missing one or more parameters required.'
+    assert response.json['message'] == 'Missing one or more parameters required.'
 
 
 def test_predict_route_missing_G2():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?failures=4&activites=0&higher=0&studytime=1&G1=10'
+    url = '/predict?failures=4&activities=0&higher_edu=0&studytime=1&G1=10'
 
     response = client.get(url)
     assert response.status_code == 400
-    assert response.json['message'] == b'Missing one or more parameters required.'
+    assert response.json['message'] == 'Missing one or more parameters required.'
 
 
 def test_predict_route_invalid_failures():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?failures=5&activites=1&higher=1&studytime=&G1=20&G2=20'
+    url = '/predict?failures=5&activities=1&higher_edu=1&studytime=&G1=20&G2=20'
 
     response = client.get(url)
     assert response.status_code == 400
-    assert response.json['message'] == b'Applicant is likely to succeed.'
+    assert response.json['message'] == 'One or more invalid parameters.'
 
 
 def test_predict_route_invalid_activities():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?failures=1&activites=2&higher=1&studytime=4&G1=20&G2=20'
+    url = '/predict?failures=1&activities=2&higher_edu=1&studytime=4&G1=20&G2=20'
 
     response = client.get(url)
     assert response.status_code == 400
-    assert response.json['message'] == b'One or more invalid parameters.'
+    assert response.json['message'] == 'One or more invalid parameters.'
 
 
 def test_predict_route_invalid_higher():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?failures=1&activites=1&higher=4&studytime=2&G1=20&G2=20'
+    url = '/predict?failures=1&activities=1&higher_edu=4&studytime=2&G1=20&G2=20'
 
     response = client.get(url)
     assert response.status_code == 400
-    assert response.json['message'] == b'One or more invalid parameters.'
+    assert response.json['message'] == 'One or more invalid parameters.'
 
 def test_predict_route_invalid_studytime():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?failures=1&activites=1&higher=0&studytime=27&G1=20&G2=20'
+    url = '/predict?failures=1&activities=1&higher_edu=0&studytime=27&G1=20&G2=20'
 
     response = client.get(url)
     assert response.status_code == 400
-    assert response.json['message'] == b'One or more invalid parameters.'
+    assert response.json['message'] == 'One or more invalid parameters.'
 
 
 def test_predict_route_invalid_G1():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?failures=1&activites=1&higher=0&studytime=1&G1=43&G2=20'
+    url = '/predict?failures=1&activities=1&higher_edu=0&studytime=1&G1=43&G2=20'
 
     response = client.get(url)
     assert response.status_code == 400
-    assert response.json['message'] == b'One or more invalid parameters.'
+    assert response.json['message'] == 'One or more invalid parameters.'
 
 
 def test_predict_route_invalid_G2():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?failures=1&activites=1&higher=0&studytime=1&G1=20&G2=26'
+    url = '/predict?failures=1&activities=1&higher_edu=0&studytime=1&G1=20&G2=26'
 
     response = client.get(url)
     assert response.status_code == 400
-    assert response.json['message'] == b'One or more invalid parameters.'
+    assert response.json['message'] == 'One or more invalid parameters.'
 
