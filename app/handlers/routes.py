73829300f0c2d5cc78d454dc2241fdc51f33a673
Lora <lbzhou@DESKTOP-UTFSENS.localdomain>
2022-11-05 14:31:00 -0400

    Added additional api features to routes

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -19,15 +19,21 @@ def configure_routes(app):
     @app.route('/predict')
     def predict():
         #use entries from the query string here but could also use json
-        age = request.args.get('age')
-        absences = request.args.get('absences')
-        health = request.args.get('health')
-        data = [[age], [health], [absences]]
+        failures = request.args.get('failures')
+        activities = request.args.get('activities')
+        higher_edu = request.args.get('higher_edu')
+        studytime = request.args.get('studytime')
+        G1 = request.args.get('G1')
+        G2 = request.args.get('G2')
+        data = [[failures], [activities], [higher_edu], [studytime], [G1], [G2]]
         query_df = pd.DataFrame({
-            'age': pd.Series(age),
-            'health': pd.Series(health),
-            'absences': pd.Series(absences)
+            'G1': pd.Series(G1),
+            'G2': pd.Series(G2),
+            'activities': pd.Series(activities),
+            'failures': pd.Series(failures),
+            'higher': pd.Series(higher_edu),
+            'studytime': pd.Series(studytime)
         })
         query = pd.get_dummies(query_df)
         prediction = clf.predict(query)
-        return jsonify(np.asscalar(prediction))
+        return jsonify(np.ndarray.item(prediction))
